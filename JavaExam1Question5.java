92acf64d448667bd27ea06e3f1fb8ca1258c8de0
mpp1221 <mpp1221@yahoo.com>
2015-06-15 17:11:11 -0500

    Exam1Question5
    Exam1Question5

---
+++ JavaExam1Question5.java
@@ -0,0 +1,23 @@
+package com.primitive;
+
+import java.util.Scanner;
+
+public class JavaExam1Question5 {
+
+	
+	public static void main(String[]args){
+		
+		Scanner mpp = new Scanner(System.in);
+		
+		System.out.print(" Please Enter your age here : ");
+		int age = mpp.nextInt();
+		if (age >= 21 && age <= 35){
+			System.out.println(" You are young ");
+		}else if ( age >=36 && age <=50){
+		System.out.println(" You are not that old");
+		}else {
+			System.out.println(" You are Senior Citizen");
+		}
+		mpp.close();
+	}
+}
